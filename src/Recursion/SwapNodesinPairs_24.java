95fba543a662b88d836c7b57fdd2e9bf245404d0
WangZT <wangzitao9@gmail.com>
2021-11-07 16:00:23 -0500

    Recursion 24-medium

---
+++ src/Recursion/SwapNodesinPairs_24.java
@@ -0,0 +1,31 @@
+package Recursion;
+
+
+
+public class SwapNodesinPairs_24 {
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode l = head;
+        swap(head);
+        return l;
+        /**
+         if (head == null || head.next == null)
+            return head;
+         ListNode t = head;
+         head = head.next;
+         t.next = swapPairs(head.next);
+         head.next = t;
+         **/
+    }
+    public void swap(ListNode head){
+        if(head == null || head.next == null){
+            return;
+        }
+        int t = head.val;
+        head.val = head.next.val;
+        head.next.val = t;
+        head = head.next;
+        swap(head.next);
+    }
+
+}
