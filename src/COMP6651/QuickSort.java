104a3991eef05fbf34ec8572216b0c28c113a4c8
WangZT <wangzitao9@gmail.com>
2022-03-20 22:38:36 -0400

    DP: leetcode72

--- src/COMP6651/QuickSort.java
+++ src/COMP6651/QuickSort.java
@@ -1,39 +1,42 @@
-package Sort.EightSort;
+package COMP6651;
 
 public class QuickSort {
-
-    public static void quickSort(int[] arr, int low, int high){
-        int i, j, temp, t;
+    public static void QuickSort(int arr[], int low, int high){
         if(low > high){
             return;
         }
-        i = low;
-        j = high;
+        int i, j, temp, t;
         temp = arr[(low + high)/2];
         int change = arr[(low + high)/2];
         arr[(low + high)/2] = arr[low];
         arr[low] = change;
+        i = low;
+        j = high;
         while(i < j){
-            while(temp <= arr[j] && i < j){
-                j--;
+            for(; j > i; j--){
+                if( arr[j] < temp){
+                    break;
+                }
             }
-            while(temp >= arr[i] && i < j){
-                i++;
+            for(; i < j; i++){
+                if(arr[i] > temp){
+                    break;
+                }
             }
             if(i < j){
-                t = arr[j];
-                arr[j] = arr[i];
-                arr[i] = t;
+                t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;
             }
         }
         arr[low] = arr[i];
         arr[i] = temp;
-        quickSort(arr, low, j-1);
-        quickSort(arr, j+1, high);
+        QuickSort(arr, low, i-1);
+        QuickSort(arr, i+1, high);
     }
     public static void main(String[] args){
         int[] arr = {29, 38, 74, 78, 24, 75, 42, 33, 21, 62, 18, 77, 30, 16};
-        quickSort(arr, 0, arr.length-1);
+        QuickSort(arr, 0, arr.length-1);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
