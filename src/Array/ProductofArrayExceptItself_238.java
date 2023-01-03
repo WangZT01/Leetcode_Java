a66ab0f02b50cda647530178540a8df41191324e
WangZT <wangzitao9@gmail.com>
2023-01-02 22:41:23 -0500

    238

--- src/Array/ProductofArrayExceptItself_238.java
+++ src/Array/ProductofArrayExceptItself_238.java
@@ -8,19 +8,17 @@ public class ProductofArrayExceptItself_238 {
         int suffix[] = new int [nums.length];
         prefix[0] = 1;
         suffix[nums.length - 1] = 1;
-        int pre = prefix[0];
-        int suf = 1;
+
         for(int i = 1; i < nums.length; i++){
-            prefix[i] = pre * nums[i - 1];
-            pre = pre * nums[i - 1];
+            prefix[i] = prefix[i - 1] * nums[i - 1];
         }
         for(int i = nums.length - 1; i > 0; i--){
-            suffix[i - 1] = suf * nums[i];
-            suf = suf * nums[i];
+            suffix[i - 1] = suffix[i - 1] * nums[i];
         }
         for(int i = 0; i < a.length; i++){
             a[i] = prefix[i] * suffix[i];
         }
         return a;
+
     }
 }
